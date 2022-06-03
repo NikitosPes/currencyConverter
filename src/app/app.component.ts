c25627e97ddc8df5bbfb2f3c3bdd43fd77ae5157
NikitosPes <kutsokon95@gmail.com>
2022-06-03 13:07:49 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from './services/currency.service';
+import { currencyRates } from './models/currency';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
+
   title = 'converter';
+  currencyRates: currencyRates = { USD: 0, EUR: 0, BTC: 0, UAH: 1}
+
+  constructor(private currencyService: CurrencyService) {}
+
+  ngOnInit(): void {
+    this.parseDataFromApi();
+  }
+
+  private parseDataFromApi(): void {
+    this.currencyService.getCurrentExchangeRate().subscribe(res => {
+      res.forEach(item => {
+        this.currencyRates[`${item.ccy}`] = item.buy;
+      })
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ConverterComponent } from './components/converter/converter.component';
+import { ExchangeFormComponent } from './components/exchange-form/exchange-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConverterComponent,
+    ExchangeFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
